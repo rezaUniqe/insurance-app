8cb52d6c55dc9012a8ce52d8525ce2344ba54dea
= <=>
2025-03-14 19:42:42 +0330

    added react query and react query kit

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {routing} from '@/i18n/routing';
+import ReactQueryProvider from "@/providers/react-query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,7 +35,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      <ReactQueryProvider>
         {children}
+      </ReactQueryProvider>
       </body>
     </html>
   );
